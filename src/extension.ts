4d313db7484818088aa7163c5c02fad55294c28a
Vignaesh Ram A <vignaesh01@gmail.com>
2021-12-26 20:26:29 +0530

    api version update

--- src/extension.ts
+++ src/extension.ts
@@ -93,7 +93,7 @@ class CodingPanel {
 	private  VERSION_END='</version>';
 	private  PACKAGE_END='</Package>';
 	private NEW_LINE ='\n';
-	private VERSION_NUM='51.0';
+	private VERSION_NUM='53.0';
 	private CHAR_TAB='\t';
 	private LOADING='*loading..';
 	private infoMsg='All metadata selected except ';
