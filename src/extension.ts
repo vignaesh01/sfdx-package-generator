210081aba7ed5d088ede6f9716b6480e44adf112
Vignaesh Ram A <vignaesh01@gmail.com>
2023-10-08 20:24:15 +0530

    api version 58

--- src/extension.ts
+++ src/extension.ts
@@ -93,7 +93,7 @@ class CodingPanel {
 	private  VERSION_END='</version>';
 	private  PACKAGE_END='</Package>';
 	private NEW_LINE ='\n';
-	private VERSION_NUM='55.0';
+	private VERSION_NUM='58.0';
 	private CHAR_TAB='\t';
 	private LOADING='*loading..';
 	private infoMsg='All metadata selected except ';
